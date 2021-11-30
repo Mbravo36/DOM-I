a4208f9f3895a53da71087c50c8329c1ddf026c3
Your Name <mari@Maribels-MBP.hsd1.tx.comcast.net>
2021-11-29 22:00:20 -0600

    finished up to bottom content

--- src/index.js
+++ src/index.js
@@ -40,3 +40,32 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
 };
 
 console.log('project wired!')
+
+// IMAGES
+const logoImg = document.querySelector('#logo-img');
+logoImg.src = siteContent.images['logo-img'];
+
+const ctaImg = document.querySelector('#cta-img');
+ctaImg.src = siteContent.images['cta-img'];
+
+const midImg = document.querySelector('#middle-img');
+midImg.src = siteContent.images['accent-img'];
+
+// FOOTER
+const footerLink = document.querySelector('footer a');
+footerLink.textContent = siteContent.footer.copyright;
+footerLink.classList.add('bold');
+
+// CONTACT
+const contact = document.querySelector('section.contact');
+contact.querySelector('h4').textContent = siteContent.contact['contact-h4'];
+contact.querySelector('p:nth-of-type(1)').textContent = siteContent.contact['address'];
+contact.querySelector('p:nth-of-type(2)').textContent = siteContent.contact['phone'];
+contact.querySelector('p:nth-of-type(3)').textContent = siteContent.contact['email'];
+
+// TOP CONTENT
+const topContent = document.querySelector('.top-content');
+topContent.children[0].children[0].textContent = siteContent['main-content']['features-h4'];
+topContent.children[0].children[1].textContent = siteContent['main-content']['features-content'];
+topContent.children[1].children[0].textContent = siteContent['main-content']['about-h4'];
+topContent.children[1].children[1].textContent = siteContent['main-content']['about-content'];
